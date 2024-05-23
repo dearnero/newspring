62c8ff76e54de6c275789fc54f61a7c1165342e6
huangfu <wx172296059@126.com>
2024-05-23 14:57:30 +0800

    四层结构

---
+++ newspirng-service/src/main/java/com/hf/left/NewspingStart.java
@@ -0,0 +1,18 @@
+package com.hf.left;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description: TODO
+ * @author: huang fu
+ * @date: 2024/5/23 14:42
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class NewspingStart{
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewspingStart.class, args);
+    }
+}
